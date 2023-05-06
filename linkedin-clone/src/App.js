eada8e170430fb89e04d138ae92b715a2747f20b
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-06 16:18:41 +0300

    application setup and clean up

---
+++ linkedin-clone/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="App-header">
+        <h1>Lets Build a LinkedIn clone</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
