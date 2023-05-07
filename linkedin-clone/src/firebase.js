1247436c13f4f79e38a52552a06815a1658515a6
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-07 09:00:16 +0300

    set connection to firebase

---
+++ linkedin-clone/src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDKOK248GhjWYThRywNGy30UGy-uytK_xE",
+  authDomain: "linkedin-clone-44c84.firebaseapp.com",
+  projectId: "linkedin-clone-44c84",
+  storageBucket: "linkedin-clone-44c84.appspot.com",
+  messagingSenderId: "226944715006",
+  appId: "1:226944715006:web:b6917a7402243e06ae7daf",
+  measurementId: "G-HKWX6LER9L"
+};
+
+const firebaseApp = firebase.initialiseApp(firebaseConfig);
