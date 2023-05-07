3de3cf30695beebfe69fa38f725fdc6c928db37f
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-07 07:43:01 +0300

    added the inputContainer

---
+++ linkedin-clone/src/InputOption.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './InputOption.css'
+
+function InputOption({Icon, title}) {
+  return (
+    <div className='inputOption'>InputOption</div>
+  )
+}
+
+export default InputOption
