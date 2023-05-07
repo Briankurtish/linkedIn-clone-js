29b867d4758974fc8fd252678abd5cf9240c894d
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-07 06:52:48 +0300

    finished the sidebar component

--- linkedin-clone/src/Sidebar.js
+++ linkedin-clone/src/Sidebar.js
@@ -3,6 +3,14 @@ import './Sidebar.css'
 import { Avatar } from '@mui/material'
 
 function Sidebar() {
+
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash">#</span>
+      <p>{topic}</p>
+    </div>
+  )
+  
   return (
     <div className='sidebar' >
       <div className="sidebar__top">
@@ -30,6 +38,12 @@ function Sidebar() {
 
       <div className="sidebar__bottom">
         <p>Recent</p>
+        {recentItem('reactjs')}
+        {recentItem('programming')}
+        {recentItem('python')}
+        {recentItem('softwareengineering')}
+        {recentItem('design')}
+        {recentItem('development')}
       </div>
     </div>
   )
