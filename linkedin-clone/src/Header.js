228356e03782e05844e8a84333db642b68620502
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-07 05:41:01 +0300

    added remaining header icons

--- linkedin-clone/src/Header.js
+++ linkedin-clone/src/Header.js
@@ -4,6 +4,9 @@ import SearchIcon from '@mui/icons-material/Search';
 import HeaderOption from './HeaderOption';
 import HomeIcon from '@mui/icons-material/Home';
 import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import BusinessCenterIcon from '@mui/icons-material/BusinessCenter';
+import ChatIcon from '@mui/icons-material/Chat';
+import NotificationsIcon from '@mui/icons-material/Notifications';
 
 function Header() {
   return (
@@ -22,6 +25,10 @@ function Header() {
       <div className='header__right'>
         <HeaderOption Icon={HomeIcon} title='Home' />
         <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
+        <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+        <HeaderOption Icon={ChatIcon} title='Messaging' />
+        <HeaderOption Icon={NotificationsIcon} title='Notification' />
+        <HeaderOption avatar= 'https://avatars.githubusercontent.com/u/67450280?v=4' title='me' />
       </div>
 
     </div>
