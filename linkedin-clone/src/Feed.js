55b2753c24b8c9fcf4d8fc70a47c1b9c167e30da
Cipher <67450280+Briankurtish@users.noreply.github.com>
2023-05-07 08:04:54 +0300

    done with the feed input container

--- linkedin-clone/src/Feed.js
+++ linkedin-clone/src/Feed.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import './Feed.css'
 import CreateIcon from '@mui/icons-material/Create'
+import ImageIcon from '@mui/icons-material/Image'
+import SubscriptionsIcon from '@mui/icons-material/Subscriptions'
+import EventNoteIcon from '@mui/icons-material/EventNote'
+import CalendarViewDayIcon from '@mui/icons-material/CalendarViewDay'
 import InputOption from './InputOption'
 
 function Feed() {
@@ -16,9 +20,15 @@ function Feed() {
         </div>
         
         <div className="feed__inputOptions">
-          <InputOption />
+          <InputOption Icon={ImageIcon} title='Photo' color='#70B5F9' />
+          <InputOption Icon={SubscriptionsIcon} title='Video' color='#E7A33E' />
+          <InputOption Icon={EventNoteIcon} title='Event' color='#C0CBCD' />
+          <InputOption Icon={CalendarViewDayIcon} title='Write Article' color='#7FC15E' />
         </div>
       </div>
+
+      {/* Posts*/}
+      
     </div>
   )
 }
--- linkedin-clone/src/InputOption.js
+++ linkedin-clone/src/InputOption.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import './InputOption.css'
 
-function InputOption({Icon, title}) {
+function InputOption({Icon, title, color}) {
   return (
-    <div className='inputOption'>InputOption</div>
+    <div className='inputOption'>
+      <Icon style={{ color: color}}/>
+      <h4>{title}</h4>
+    </div>
   )
 }
 
